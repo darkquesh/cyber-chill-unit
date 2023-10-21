469e9a8a4ee09f8c89e3a069583f9b263015e6af
darkquesh <erenkaradag0001@hotmail.com>
2023-10-21 22:09:54 +0300

    Added few camera configs

--- grad_proj_button.py
+++ grad_proj_button.py
@@ -1,33 +1,54 @@
 # import module
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from selenium.webdriver import ActionChains
 import time
 import requests
 import os
 
-os.chdir("/home/nitro/Desktop/image_proc/Detic/")
+from run_imgs import run_imgs
+
+main_dir = "C:\\Users\\erenk\\OneDrive\\Desktop\\eren\\ELE401_402\\Detic\\Detic\\"
+os.chdir(main_dir) # Windows
+#os.chdir("/home/nitro/Desktop/image_proc/Detic/")  # Linux
   
 # Create the webdriver object. Here the 
 # chromedriver is present in the driver 
 # folder of the root directory.
 driver = webdriver.Firefox()
   
-# get webpage
-driver.get("http://10.42.0.206")
+# Camera web server - change it accordingly!
+cam_server = "http://192.168.1.26"
+
+# Get webpage
+driver.get(cam_server)
 
 # Maximize the window and let code stall 
 # for 2s to properly maximise the window.
 #driver.maximize_window()
 time.sleep(2)
 
-select=Select(driver.find_element("xpath" ,"//select[@class='default-action']"))
-select.select_by_index(0)
+# Select VGA 640x480 resolution
+select = Select(driver.find_element("xpath" ,"//select[@id='framesize']"))
+select.select_by_value("8")
 time.sleep(0.5)
 
-image_url = 'http://10.42.0.206/capture'
+# Increase gain ceiling (exposure)
+en = driver.find_element("xpath" ,"//input[@id='gainceiling']")
+move = ActionChains(driver)
+move.click_and_hold(en).move_by_offset(4, 0).release().perform()
+time.sleep(1)
 
+# Get the image
+image_url = f'{cam_server}/capture'
 img_data = requests.get(image_url).content
-with open('ESP/image2.jpg', 'wb') as handler:
+
+try:  
+    os.mkdir('ESP32')  
+except OSError as error:  
+    print(error)
+
+with open('ESP32/image2.jpg', 'wb') as handler:
     handler.write(img_data)
 
 ###
@@ -37,25 +58,17 @@ with open('ESP/image2.jpg', 'wb') as handler:
 # Change the directory according to your machine (though it must be in your Detic directory)
 #os.chdir("/home/nitro/Desktop/image_proc/Detic/")
 
-# set the command to execute
-command1 = "python3 demo.py --config-file configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml"
-command2 =  "--vocabulary lvis --opts MODEL.WEIGHTS models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth"
-
 # set the input and output directories
-input_dir = "/home/nitro/Desktop/image_proc/Detic/ESP/"
-output_dir = "/home/nitro/Desktop/image_proc/Detic/ESP/runs/1/"
+input_dir = "ESP32\\"
+output_dir = "ESP32\\runs\\"
+file_extension = ".jpg"
 
-# iterate through all the files in the input directory
-for i, filename in enumerate(sorted(os.listdir(input_dir))):#, key=lambda f: int(''.join(filter(str.isdigit, f))))):
-    #print(i+1, filename)
-    
-    if filename.endswith(".jpg"):
-        # set the input and output file paths
-        input_file = os.path.join(input_dir, filename)
-        output_file = os.path.join(output_dir, "out{}.jpg".format(i+1))
+try:  
+    os.mkdir(output_dir)  
+except OSError as error:  
+    print(error)
 
-        # execute the command with the input and output file paths
-        os.system("{} --input {} --output {} {}".format(command1, input_file, output_file, command2))
+run_imgs(main_dir, input_dir, output_dir, file_extension)
 
 
 '''  
