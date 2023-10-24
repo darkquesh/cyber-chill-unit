2a5166f4e00eea405ea6679119c327be3704b3fc
darkquesh <erenkaradag0001@hotmail.com>
2023-10-24 22:06:53 +0300

    Modular code

--- uploadFiles.py
+++ uploadFiles.py
@@ -1,10 +1,16 @@
 import requests
 
-url = 'http://51.20.72.77/php-scripts/upload.php'  # Replace with your server's URL
-filename = 'orange.jpg'
-files = {'file': (filename, open(filename, 'rb'))}
-data = {'folder': '--raw'}  # Specify the folder parameter here
+def uploadFile(srv_url: str, filename: str, param: str):
+    files = {'file': (filename, open(filename, 'rb'))}
+    data = {'folder': param}  # Specify the folder parameter here
 
-response = requests.post(url, files=files, data=data)
+    response = requests.post(srv_url, files=files, data=data)
+    
+    print(response.text)
 
-print(response.text)
+if __name__ == "__main__":
+    srv_url = 'http://51.20.72.77/php-scripts/upload.php'  # Replace with your server's URL
+    filename = 'orange.jpg'
+    param = '--raw'
+
+    uploadFile(srv_url, filename, param)
